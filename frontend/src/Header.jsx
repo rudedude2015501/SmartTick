04153aa772d03b87a41782a839190f7ff5995807
Greg <4gregorygrigorian@gmail.com>
2025-04-15 15:13:32 -0700

    Created header

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ backgroundColor: 'black' }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header
