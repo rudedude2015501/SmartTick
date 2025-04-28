fd74e56d0dbcf25399951de827760db83d6a35ee
Greg <4gregorygrigorian@gmail.com>
2025-04-28 14:01:11 -0700

    Refactored

--- backend/app/scraper.py
+++ backend/app/scraper.py
@@ -1,26 +1,18 @@
-#Rudy CHavez 4/27/25
-#A "webscraper" using a unique api to get regular data for free, using requests and beautiful soup 4
 import requests
-from bs4 import BeautifulSoup
 import json
 
 URL = "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
 
-#gets the political data from the above link and returns a python or JSON file:
-def getPolData(RETURNTYPE:str):
-    rData = requests.get(URL) #GET request
-    soup = BeautifulSoup(rData.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-    # print(soup.prettify())
-    if rData.status_code != 200:
-        exit("Error with URL requests" + str(rData.status_code))
-    #content returned is a JSON object containg a list of dictionaries, each containing the recent transaction
-    if RETURNTYPE == "PY":
-        return (json.loads(rData.content))
-    elif RETURNTYPE == "JS":
-        return (rData.content)
-
-
-#testing
-def test_scraper():
-    print("testing scraper.py:")
+def getPolData(RETURNTYPE: str):
+    # Fetch the data from the API
+    response = requests.get(URL)
+    if response.status_code != 200:
+        raise Exception(f"Error with URL requests: {response.status_code}")
     
+    # Return data in the requested format
+    if RETURNTYPE == "PY":
+        return response.json()  # Return Python object
+    elif RETURNTYPE == "JS":
+        return response.text  # Return raw JSON string
+    else:
+        raise ValueError("Invalid RETURNTYPE. Use 'PY' or 'JS'.")
