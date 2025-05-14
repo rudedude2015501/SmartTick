1e7b2da4b1d98517ea0d132a43b09829739fa250
Antonio Duran <aduranra@macmini.local>
2025-05-13 18:38:42 -0700

    fixed return app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -274,8 +274,6 @@ def create_app():
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
-    return app
-
     @app.route('/api/autocomplete/stocks', methods=["GET"])
     def autocomplete_stocks():
         """
@@ -308,3 +306,5 @@ def create_app():
         except Exception as e:
             app.logger.error(f"error with autocomplete: {e}", exc_info=True)
             return jsonify([]), 500
+    
+    return app
