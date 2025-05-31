ec7a21ec80b3be7a97bdad026489eadb4f90294d
Greg <4gregorygrigorian@gmail.com>
2025-05-31 00:05:52 -0700

    Deleted file

--- frontend/src/tests/sample.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from '../App.jsx'
-
-describe('Sample Test Case', () => {
-    it('render app title', () => {
-        render(<App />)
-        expect(screen.getByText('SmartTick')).toBeInTheDocument()
-    })
-})
